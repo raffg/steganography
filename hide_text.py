16f51fa508985c4bf9a97c6579482eff3642dbe8
Greg Rafferty <greg.rafferty+git@gmail.com>
2018-06-12 16:08:10 -0700

    add files

---
+++ hide_text.py
@@ -0,0 +1,128 @@
+from PIL import Image
+
+
+def encrypt(image_path, text_path):
+    '''
+    Encrypt a string inside an image using the least significant bits method
+    INPUT: string, a path to an image file; string, a path to a text file
+    OUTPUT: a bitmap image saved in the root folder with _hidden.bmp appended to the filename
+    '''
+
+    # check if image is already a bitmap
+    if image_path[-4:] != '.bmp':
+        img = Image.open(image_path)
+        image_path = image_path[:-4] + '.bmp'
+        img.save(image_path)
+
+    with open(image_path[:-4] + '.bmp', 'rb') as bmp_file:
+        bmp = bmp_file.read()
+
+    with open(text_path, 'rb') as to_hide_file:
+        msg = to_hide_file.read()
+
+    # append the length of the message to assist with decoding
+    temp = msg.decode('utf-8')
+    msg = bytearray(str(len(temp)) + '\n' + temp, 'utf-8')
+
+    # color data begins at the byte at position 10
+    start_offset = bmp[10]
+
+    bmpa = bytearray(bmp)
+
+    # convert the msg in bytes to bits
+    bits = []
+    for i in range(len(msg)):
+        # a byte can at max be 8 digits long, i.e. 0b11111111 = 255
+        # we start at the left most bit (position 7) and work down to 0
+        for j in range(7, -1, -1):
+            #Create the logic array of bits for our data
+            bits.append(nth_bit_present(msg[i], j))
+
+    data_array = bits
+
+    # ensure the image is large enough to contain the text
+    assert len(data_array) < len(bmpa) + start_offset
+
+    for i in range(len(data_array)):
+        bmpa[i + start_offset] = set_final_bit(bmpa[i + start_offset], 
+                                               data_array[i])
+
+    with open(image_path.replace('.bmp', '_hidden.bmp'), 'wb') as out:
+        out.write(bmpa)
+
+
+def decrypt(image_path):
+    with open(image_path, 'rb') as bmp_file:
+        bmp = bmp_file.read()
+
+    # color data begins with the byte at position 10
+    start_offset = bmp[10]
+
+    # deconstruct each byte and get its final bit
+    bits = []
+    for i in range(start_offset, len(bmp)):
+        bits.append(nth_bit_present(bmp[i], 0))
+
+    # combine our bit array into bytes
+    out_bytes = []
+    for i in range(0, len(bits), 8):
+        if(len(bits) - i > 8):
+            out_bytes.append(bits_to_byte(bits[i: i + 8]))
+
+    # convert bytes to characters
+    out = []
+    for b in out_bytes:
+        out.append(chr(b))
+
+    output = ''.join(out)
+
+    # strip out the first line containing the length of the message
+    idx = output.find('\n')
+    msg_len = int(output[:idx])
+
+    # ignore data after the message is complete
+    msg = output[idx + 1: idx + msg_len + 1]
+
+    with open("hidden_message.txt", "w") as text_file:
+        text_file.write(msg)
+
+    print(msg)
+
+
+def nth_bit_present(my_byte, n):
+    # bitwise check to see what the nth bit is
+    # if anything other than 0, it is TRUE else FALSE
+    return (my_byte & (1 << n)) != 0
+
+
+def set_final_bit(my_byte, ends_in_one):
+    new_byte = 0
+    if ends_in_one:
+        if(nth_bit_present(my_byte, 0)):
+            # byte already ends in 1
+            new_byte = my_byte
+        else:
+            new_byte = my_byte + 1
+    else:
+        if(nth_bit_present(my_byte, 0)):
+            new_byte = my_byte - 1
+        else:
+            # byte already ends in 0
+            new_byte = my_byte
+    return new_byte
+
+
+def bits_to_byte(bits):
+    # convert 8 bits into 1 byte
+    assert len(bits) == 8
+    new_byte = 0
+    for i in range(8):
+        if bits[i]:
+            # this bit == 1 and the "position" we are at in the byte is 7-i
+            # bitwise OR will insert a 1 a this position
+            new_byte |= 1 << 7 - i
+        else:
+            # this bit == 0 and the "position" we are at in the byte is 7-i
+            # bitwise OR will insert a 0 a this position
+            new_byte |= 0 << 7 - i
+    return new_byte
---
+++ image_merge.py
@@ -0,0 +1,120 @@
+from PIL import Image
+
+
+def merge(img1, img2):
+    '''
+    Merge two images. The second one will be merged into the first one.
+    INPUT: First image, Second image
+    OUTPUT: A new merged image.
+    '''
+
+    # Ensure image 1 is larger than image 2
+    if img2.size[0] > img1.size[0] or img2.size[1] > img1.size[1]:
+        raise ValueError('Image 1 size is lower than image 2 size!')
+
+    # Get the pixel map of the two images
+    pixel_map1 = img1.load()
+    pixel_map2 = img2.load()
+
+    # Create a new image that will be outputted
+    new_image = Image.new(img1.mode, img1.size)
+    pixels_new = new_image.load()
+
+    for i in range(img1.size[0]):
+        for j in range(img1.size[1]):
+            rgb1 = integer_to_binary(pixel_map1[i, j])
+
+            # Use a black pixel as default
+            rgb2 = integer_to_binary((0, 0, 0))
+
+            # Check if the pixel map position is valid for the second image
+            if i < img2.size[0] and j < img2.size[1]:
+                rgb2 = integer_to_binary(pixel_map2[i, j])
+
+            # Merge the two pixels and convert it to a integer tuple
+            rgb = merge_rgb(rgb1, rgb2)
+
+            pixels_new[i, j] = binary_to_integer(rgb)
+
+    return new_image
+
+
+def unmerge(img):
+    '''
+    Unmerge an image.
+    INPUT: The input image.
+    OUTPUT: The unmerged/extracted image.
+    '''
+
+    # Load the pixel map
+    pixel_map = img.load()
+
+    # Create the new image and load the pixel map
+    new_image = Image.new(img.mode, img.size)
+    pixels_new = new_image.load()
+
+    # Tuple used to store the image original size
+    original_size = img.size
+
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            # Get the RGB (as a string tuple) from the current pixel
+            r, g, b = integer_to_binary(pixel_map[i, j])
+
+            # Extract the last 4 bits (corresponding to the hidden image)
+            # Concatenate 4 zero bits because we are working with 8 bit values
+            rgb = (r[4:] + "0000",
+                   g[4:] + "0000",
+                   b[4:] + "0000")
+
+            # Convert it to an integer tuple
+            pixels_new[i, j] = binary_to_integer(rgb)
+
+            # If this is a 'valid' position, store it
+            # as the last valid position
+            if pixels_new[i, j] != (0, 0, 0):
+                original_size = (i + 1, j + 1)
+
+    # Crop the image based on the 'valid' pixels
+    new_image = new_image.crop((0, 0, original_size[0], original_size[1]))
+
+    return new_image
+
+
+def integer_to_binary(rgb):
+    '''
+    Convert RGB pixel values from integer to binary
+    INPUT: An integer tuple (e.g. (220, 110, 96))
+    OUTPUT: A string tuple (e.g. ("00101010", "11101011", "00010110"))
+    '''
+    r, g, b = rgb
+    return ('{0:08b}'.format(r),
+            '{0:08b}'.format(g),
+            '{0:08b}'.format(b))
+
+
+def binary_to_integer(rgb):
+    '''
+    Convert RGB pixel values from binary to integer.
+    INPUT: A string tuple (e.g. ("00101010", "11101011", "00010110"))
+    OUTPUT: Return an int tuple (e.g. (220, 110, 96))
+    '''
+    r, g, b = rgb
+    return (int(r, 2),
+            int(g, 2),
+            int(b, 2))
+
+
+def merge_rgb(rgb1, rgb2):
+    '''
+    Merge two RGB pixels using 4 least significant bits.
+    INPUT: A string tuple (e.g. ("00101010", "11101011", "00010110")),
+           Another string tuple (e.g. ("00101010", "11101011", "00010110"))
+    OUTPUT: An integer tuple with the two RGB values merged
+    '''
+    r1, g1, b1 = rgb1
+    r2, g2, b2 = rgb2
+    rgb = (r1[:4] + r2[:4],
+           g1[:4] + g2[:4],
+           b1[:4] + b2[:4])
+    return rgb
